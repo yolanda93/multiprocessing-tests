68143c2815925111e29e7428cbbc5a389d740744
yolanda93 <yolanda93h@gmail.com>
2017-05-23 10:53:55 +0200

    manager shared state multiplatform

--- test_managers.py
+++ test_managers.py
@@ -1,17 +1,52 @@
-import multiprocessing
+from multiprocessing import Process, Manager, freeze_support
 
-def f(ns):
-    ls.append(8)
-    ls.append(8)
 
+def test_sharedObject(ns):
+    ns.x += 1
+    ns.y.append(2)
+    ns.ls.append(4)
+
+
+def test_inNamespace(ns):
+    ns.x += 1
+    ns.ls.append(8)
+    ns.ls.append(8)
+
+    tmp = ns.y  # retrieve the shared value
+    tmp.append(10)
+    ns.y = tmp
+
+    tmp = ns.ls
+    tmp.append(8)
+    ns.ls = tmp
 
 
 if __name__ == '__main__':
-    manager = multiprocessing.Manager()
-    ls = manager.list([1, 1, 1])
+    manager = Manager()
+    ns = manager.Namespace()
+    ns.x = 1
+    ns.y = [1]
+    ns.ls = manager.list([1, 1, 1])
 
-    print 'before', ls
-    p = multiprocessing.Process(target=f, args=(ls,))
+    """Single process example"""
+
+    print 'before', ns.ls, ns.x, ns.y
+    p = Process(target=test_inNamespace, args=(ns,))
     p.start()
     p.join()
-    print 'after', ls
+    print 'after',  ns.ls, ns.x, ns.y
+
+    """Multiple process example"""
+
+    n=10
+    ns.x = 1
+    print 'before', ns.ls, ns.x, ns.y
+    p = [Process(target=test_inNamespace, args=(ns,)) for i in range(n -1)]
+
+    for each in p:
+        each.start()
+
+    for each in p:
+        each.join()
+
+    print 'after',  ns.ls, ns.x, ns.y
